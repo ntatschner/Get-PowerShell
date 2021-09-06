87208d77c8b7ac121dd1770144427492a948138a
Nigel Tatschner <Nigel.Tatschner@Take2Games.com>
2021-09-06 14:23:09 +0100

    updates

--- Modules/Tatux.Utils/Private/Tests/ConvertFrom-FileTime.Tests.ps1
+++ Modules/Tatux.Utils/Private/Tests/ConvertFrom-FileTime.Tests.ps1
@@ -19,7 +19,7 @@ Describe -Tags 'PSSA' -Name 'Testing against PSScriptAnalyzer rules' {
 		    {
 			    It "Function $FunctionFileName should pass $Rule" {
 				    $Failures = $AnalyzerIssues | Where-Object -Property RuleName -EQ -Value $rule
-				    ($Failures | Measure-Object).Count | Should Be 0
+				    ($Failures | Measure-Object).Count | Should -Be 0
 			    }
 		    }
 		    else
@@ -27,7 +27,7 @@ Describe -Tags 'PSSA' -Name 'Testing against PSScriptAnalyzer rules' {
 			    # We still want it in the tests, but since it doesn't actually get tested we will skip
 			    It "Function $FunctionFileName should pass $Rule" -Skip {
 				    $Failures = $AnalyzerIssues | Where-Object -Property RuleName -EQ -Value $rule
-				    ($Failures | Measure-Object).Count | Should Be 0
+				    ($Failures | Measure-Object).Count | Should -Be 0
 			    }
 		    }
 		
--- Modules/Tatux.Utils/Private/Tests/Split-HUUri.Tests.ps1
+++ Modules/Tatux.Utils/Private/Tests/Split-HUUri.Tests.ps1
@@ -25,7 +25,7 @@ Describe -Tags 'PSSA' -Name 'Testing against PSScriptAnalyzer rules' {
 		    {
 			    It "Function $FunctionFileName should pass $Rule" {
 				    $Failures = $AnalyzerIssues | Where-Object -Property RuleName -EQ -Value $rule
-				    ($Failures | Measure-Object).Count | Should Be 0
+				    ($Failures | Measure-Object).Count | Should -Be 0
 			    }
 		    }
 		    else
@@ -33,7 +33,7 @@ Describe -Tags 'PSSA' -Name 'Testing against PSScriptAnalyzer rules' {
 			    # We still want it in the tests, but since it doesn't actually get tested we will skip
 			    It "Function $FunctionFileName should pass $Rule" -Skip {
 				    $Failures = $AnalyzerIssues | Where-Object -Property RuleName -EQ -Value $rule
-				    ($Failures | Measure-Object).Count | Should Be 0
+				    ($Failures | Measure-Object).Count | Should -Be 0
 			    }
 		    }
 		
