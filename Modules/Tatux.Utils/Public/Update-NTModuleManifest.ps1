ceb298b38b265663c1bf06fcf1e7f3e75c0409c5
Nigel Tatschner <ntatschner@gmail.com>
2024-04-04 20:21:18 +0100

    Fixed update module

--- Modules/Tatux.Utils/Public/Update-NTModuleManifest.ps1
+++ Modules/Tatux.Utils/Public/Update-NTModuleManifest.ps1
@@ -18,12 +18,10 @@ function Update-NTModuleManifest {
         if ([string]::IsNullOrEmpty($ManifestFileContent -match '[\s|\t]*CmdletsToExport\s*=.*') -eq $false) {
             $LineWithCmdletsToExport = $($ManifestFileContent | Select-String -Pattern '^[\s|\t]*CmdletsToExport\s*=(.*)$')
             $LineNumber = $LineWithCmdletsToExport.LineNumber - 1
-            $ManifestFileLines = $ManifestFileContent -split "`n"
-            $CmdletsToExport = $(Get-ChildItem -Path $PublicFunctions -Filter "*.ps1" | Where-Object { $_.Name -notlike "*.tests.ps1" | select Name }) -join ', '
-            $NewLine = "CmdletsToExport = @($CmdletsToExport)"
-            Write-Output "Replacing line: $LineWithCmdletsToExport with $NewLine"
-            $ManifestFileContent = $ManifestFileLines[$LineNumber] -replace [regex]::Escape($LineWithCmdletsToExport.Matches.Groups[1].Value), $NewLine
-            $ManifestFileContent = $ManifestFileLines -join "`n"
+            $CmdletsToExport = $(Get-ChildItem -Path $PublicFunctions -Filter "*.ps1" | Where-Object { $_.Name -notlike "*.tests.ps1" } | Select-Object -ExpandProperty BaseName ) -join ', '
+            $NewLine = " @($CmdletsToExport)"
+            Write-Output "Replacing line: $($LineWithCmdletsToExport.Matches.Groups[1].Value) with $NewLine"
+            $ManifestFileContent[$LineNumber] = $ManifestFileContent[$LineNumber] -replace [regex]::Escape($LineWithCmdletsToExport.Matches.Groups[1].Value), $NewLine
             Set-Content -Path $Path -Value $ManifestFileContent
         } else {
             Write-Error "CmdletsToExport field could not be found in the manifest file."
