7c906761b77e671a803b2e51f739e74d95955a8a
Nigel Tatschner <ntatschner@gmail.com>
2024-04-04 13:07:02 +0100

    Update Update-NTModuleManifest.ps1

--- Modules/Tatux.Utils/Public/Update-NTModuleManifest.ps1
+++ Modules/Tatux.Utils/Public/Update-NTModuleManifest.ps1
@@ -2,11 +2,16 @@ function Update-NTModuleManifest {
     [CmdletBinding()]
     param(
     [ValidateScript({
-        if () {} else {}
-)]
+        if (-Not (Test-Path -Path $_)) {
+            throw "Enter a valid path."
+        } else {
+            $true
+        }
+    )]
     [string]$Path
 
     )
+# Test if paths a valid manifest file and if so has the right field
 
 
 }
