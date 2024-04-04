bd2fccb2ad1e8448698cf36d916bfb5233d184dc
Nigel Tatschner <ntatschner@gmail.com>
2024-04-04 19:15:24 +0100

    Updates

--- Modules/Tatux.Utils/Public/Update-NTModuleManifest.ps1
+++ Modules/Tatux.Utils/Public/Update-NTModuleManifest.ps1
@@ -1,21 +1,32 @@
 function Update-NTModuleManifest {
     [CmdletBinding()]
     param(
-    [ValidateScript({
-        if (-Not (Test-Path -Path $_) -or $_.EndsWith(".psd1") -eq $false) {
-            throw "Enter a valid manifest file."
+        [ValidateScript({
+                if (-Not (Test-Path -Path $_) -or $_.EndsWith(".psd1") -eq $false) {
+                    throw "Enter a valid manifest file."
+                }
+                else {
+                    $true
+                }
+            })]
+            [string]$Path
+        )
+        # Test if field CmdletsToExport exists in the manifest file
+        $ManifestFileContent = Get-Content -Path $Path
+        $ModuleRoot = Split-Path -Path $Path -Parent
+        $PublicFunctions = Join-Path -Path $ModuleRoot -ChildPath "Public"
+        if ([string]::IsNullOrEmpty($ManifestFileContent -match '[\s|\t]*CmdletsToExport\s*=.*') -eq $false) {
+            $LineWithCmdletsToExport = $($ManifestFileContent | Select-String -Pattern '^[\s|\t]*CmdletsToExport\s*=(.*)$')
+            $LineNumber = $LineWithCmdletsToExport.LineNumber - 1
+            $ManifestFileLines = $ManifestFileContent -split "`n"
+            $CmdletsToExport = $(Get-ChildItem -Path $PublicFunctions -Filter "*.ps1" | Where-Object { $_.Name -notlike "*.tests.ps1" | select Name }) -join ', '
+            $NewLine = "CmdletsToExport = @($CmdletsToExport)"
+            Write-Output "Replacing line: $LineWithCmdletsToExport with $NewLine"
+            $ManifestFileContent = $ManifestFileLines[$LineNumber] -replace [regex]::Escape($LineWithCmdletsToExport.Matches.Groups[1].Value), $NewLine
+            $ManifestFileContent = $ManifestFileLines -join "`n"
+            Set-Content -Path $Path -Value $ManifestFileContent
         } else {
-            $true
+            Write-Error "CmdletsToExport field could not be found in the manifest file."
+            break
         }
-    )]
-    [string]$Path
-
-    )
-# Test if paths a valid manifest file and if so has the right field FunctionsToExport
-    $ManifestFileContent = Get-Content -Path $Path -Raw
-
-    if ($ManifestFileContent -match "FunctionsToExport =") {
-        $LineToReplace = $ManifestFileContent | Select-String -Pattern "^FunctionsToExport = .*$"
-    }
-
 }
