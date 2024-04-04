b36d3b6fdac2e1329011da183716f65f93f45e40
Nigel Tatschner <ntatschner@gmail.com>
2024-04-04 13:35:54 +0100

    Update Update-NTModuleManifest.ps1

--- Modules/Tatux.Utils/Public/Update-NTModuleManifest.ps1
+++ Modules/Tatux.Utils/Public/Update-NTModuleManifest.ps1
@@ -2,8 +2,8 @@ function Update-NTModuleManifest {
     [CmdletBinding()]
     param(
     [ValidateScript({
-        if (-Not (Test-Path -Path $_)) {
-            throw "Enter a valid path."
+        if (-Not (Test-Path -Path $_) -or $_.EndsWith(".psd1") -eq $false) {
+            throw "Enter a valid manifest file."
         } else {
             $true
         }
@@ -12,6 +12,10 @@ function Update-NTModuleManifest {
 
     )
 # Test if paths a valid manifest file and if so has the right field FunctionsToExport
+    $ManifestFileContent = Get-Content -Path $Path -Raw
 
+    if ($ManifestFileContent -match "FunctionsToExport =") {
+        $LineToReplace = $ManifestFileContent | Select-String -Pattern "^FunctionsToExport = .*$"
+    }
 
 }
